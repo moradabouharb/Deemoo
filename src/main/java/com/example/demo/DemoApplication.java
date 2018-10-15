64b40cff6f351209d10017dacf4b8f7ec9c02740
morad abouharb <morad.abou.harb@gmail.com>
2018-10-16 01:44:24 +0200

    Add h2 database
    Add h2 configuration and dependencies

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,3 +11,5 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
+
+
